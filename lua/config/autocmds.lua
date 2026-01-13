f60f339e1fb9eccf8d6ed4bb005627addcde0509
Antoine QUERU <antoine.queru@provexi.fr>
2026-01-13 14:16:44 +0100

    Feat : add CopyPath function

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,41 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+--
+--
+vim.api.nvim_create_user_command("CopyPath", function(context)
+  local full_path = vim.fn.glob("%:p")
+
+  local file_path = nil
+  if context["args"] == "nameonly" then
+    file_path = vim.fn.fnamemodify(full_path, ":t")
+  end
+
+  -- get the file path relative to project root
+  if context["args"] == "relative" then
+    local project_marker = { ".git", "pyproject.toml" }
+    local project_root = vim.fs.root(0, project_marker)
+    if project_root == nil then
+      vim.print("can not find project root")
+      return
+    end
+
+    file_path = string.gsub(full_path, project_root, "<project-root>")
+  end
+
+  if context["args"] == "absolute" then
+    file_path = full_path
+  end
+
+  vim.fn.setreg("+", file_path)
+  vim.print("Filepath copied to clipboard!")
+end, {
+  bang = false,
+  nargs = 1,
+  force = true,
+  desc = "Copy current file path to clipboard",
+  complete = function()
+    return { "nameonly", "relative", "absolute" }
+  end,
+})
