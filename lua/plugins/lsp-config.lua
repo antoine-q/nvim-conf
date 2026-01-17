8911bd931b66fef435e824a44aa9d69717b2f258
Antoine <antoine.queru@gmail.com>
2026-01-17 17:25:31 +0100

    Feat : Add lsp and treesitter

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+      },
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "pyre", "postgres_lsp", "intelephense" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.pylsp.setup({})
+      lspconfig.postgres_lsp.setup({})
+      lspconfig.intelephense.setup({})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
