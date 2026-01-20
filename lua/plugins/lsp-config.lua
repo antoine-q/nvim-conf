7ce9578f997f57003130a04c1dc594a06bb4cd78
Antoine Queru <antoine@queru.fr>
2026-01-20 08:05:46 +0100

    Fix : LSP

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -7,6 +7,7 @@ return {
         "shellcheck",
         "shfmt",
         "flake8",
+        "pyright",
       },
     },
   },
@@ -14,7 +15,7 @@ return {
     "mason-org/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyre", "postgres_lsp", "intelephense" },
+        ensure_installed = { "lua_ls", "pyright", "postgres_lsp", "intelephense", "ruff" },
       })
     end,
   },
@@ -24,7 +25,8 @@ return {
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({ capabilities = capabilities })
-      lspconfig.pylsp.setup({ capabilities = capabilities })
+      lspconfig.pyright.setup({ capabilities = capabilities })
+      lspconfig.ruff.setup({ capabilities = capabilities })
       lspconfig.postgres_lsp.setup({ capabilities = capabilities })
       lspconfig.intelephense.setup({ capabilities = capabilities })
       lspconfig.html.setup({})
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,7 +9,7 @@ return {
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.blade_formatter,
-        null_ls.builtins.formatting.pgformatter,
+        null_ls.builtins.formatting.sqlfmt,
         null_ls.builtins.formatting.phpcsfixer,
         null_ls.builtins.formatting.djlint,
         null_ls.builtins.diagnostics.erb_lint,
