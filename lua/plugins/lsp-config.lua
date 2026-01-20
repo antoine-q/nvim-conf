693d29b1ba6b7448ecc87aded30639d32d9acea6
Antoine Queru <antoine@queru.fr>
2026-01-20 16:52:30 +0100

    Fix : double pyright in lspconfig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,39 +1,35 @@
 return {
   {
     "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "flake8",
-        "pyright",
-      },
-    },
   },
   {
     "mason-org/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "postgres_lsp", "intelephense", "ruff" },
+        ensure_installed = { "lua_ls", "pyright", "intelephense" },
       })
     end,
   },
   {
     "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({ capabilities = capabilities })
-      lspconfig.pyright.setup({ capabilities = capabilities })
-      lspconfig.ruff.setup({ capabilities = capabilities })
-      lspconfig.postgres_lsp.setup({ capabilities = capabilities })
-      lspconfig.intelephense.setup({ capabilities = capabilities })
-      lspconfig.html.setup({})
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Show definition" })
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Show references" })
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Show code actions" })
-    end,
+    opts = {
+      servers = {
+        lua_ls = {},
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                extraPaths = { vim.fn.getcwd() },
+                venvPath = ".",
+                venv = ".venv",
+              },
+            },
+          },
+        },
+        postgres_lsp = {},
+        intelephense = {},
+        html = {},
+      },
+    },
   },
 }
